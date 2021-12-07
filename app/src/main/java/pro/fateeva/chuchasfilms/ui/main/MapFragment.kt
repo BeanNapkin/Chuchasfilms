42354926e218eda433ecbf64f80e7d5c465cf484
Margarita Fateeva <margarita@fateev.pro>
2021-12-08 00:10:59 +0300

    Add searching address on map

--- app/src/main/java/pro/fateeva/chuchasfilms/ui/main/MapFragment.kt
+++ app/src/main/java/pro/fateeva/chuchasfilms/ui/main/MapFragment.kt
@@ -2,10 +2,10 @@ package pro.fateeva.chuchasfilms.ui.main
 
 import android.Manifest
 import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,21 +13,32 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import kotlinx.android.synthetic.main.map_fragment.*
 import pro.fateeva.chuchasfilms.R
+import pro.fateeva.chuchasfilms.databinding.MapFragmentBinding
+import java.io.IOException
 
 class MapFragment : Fragment() {
 
+    private var _binding: MapFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var map: GoogleMap
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        map = googleMap
+    }
+
     private val permissionResult =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
-            if (result) {
-                getLocation()
-            } else {
+            if (!result) {
                 Toast.makeText(context, "Need permission", Toast.LENGTH_SHORT)
             }
         }
@@ -36,19 +47,14 @@ class MapFragment : Fragment() {
         fun newInstance() = MapFragment()
     }
 
-    private val callback = OnMapReadyCallback { googleMap ->
-       val locationListener = LocationListener {
-//            googleMap.addMarker(MarkerOptions().position(it.latitude, it.longitude).title("You are here"))
-//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(it.latitude, it.longitude))
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        _binding = MapFragmentBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -56,6 +62,13 @@ class MapFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
         checkPermission()
+
+        initialSearchAddress()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     private fun checkPermission() {
@@ -66,36 +79,52 @@ class MapFragment : Fragment() {
         permissionResult.launch(Manifest.permission.ACCESS_FINE_LOCATION)
     }
 
-    private fun getLocation() {
-//        activity?.let { context ->
-//            if (ContextCompat.checkSelfPermission(
-//                    context,
-//                    Manifest.permission.ACCESS_FINE_LOCATION
-//                ) ==
-//                PackageManager.PERMISSION_GRANTED
-//            ) {
-//
-//                val locationManager =
-//                    context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-//                    val provider = locationManager.getProvider(LocationManager.GPS_PROVIDER)
-//                    provider?.let {
-//                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
-//                            0f, locationListener);
-//                    }
-//                } else {
-//                    val location =
-//                        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-//                    if (location == null) {
-//                        Toast.makeText(context, "No lastknown location", Toast.LENGTH_SHORT)
-//                    } else {
-//                        Toast.makeText(context, "GPS turned off", Toast.LENGTH_SHORT)
-//                    }
-//                }
-//            } else {
-//                Toast.makeText(context, "Need permission", Toast.LENGTH_SHORT)
-//            }
-//        }
+    private fun initialSearchAddress() {
+        binding.searchButton.setOnClickListener {
+            val geoCoder = Geocoder(it.context)
+            val searchText = searchEditText.text.toString()
+            Thread {
+                try {
+                    val addresses = geoCoder.getFromLocationName(searchText, 1)
+                    if (addresses.size > 0) {
+                        goToAddress(addresses, it, searchText)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+    }
+
+    private fun goToAddress(
+        addresses: MutableList<Address>,
+        view: View,
+        searchText: String
+    ) {
+        val location = LatLng(
+            addresses[0].latitude,
+            addresses[0].longitude
+        )
+        view.post {
+            setMarker(location, searchText)
+            map.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    location,
+                    17f
+                )
+            )
+        }
+    }
+
+    private fun setMarker(
+        location: LatLng,
+        searchText: String
+    ): Marker? {
+        return map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title(searchText)
+        )
     }
 
 }
